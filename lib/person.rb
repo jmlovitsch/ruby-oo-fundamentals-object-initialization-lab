486d2b7bf5a6124102a768c2d9f043e259f839b4
john <jmlovitsch@gmail.com>
2020-09-15 10:39:38 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+
+# class Dog
+#     def initialize(breed)
+#       @breed = breed
+#     end
+
+#     def breed=(breed)
+#       @breed = breed
+#     end
+
+#     def breed
+#       @breed
+#     end
+#   end
